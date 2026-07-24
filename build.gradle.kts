a6817e490da417b22452d6422774718ed9374e3c
Swaroop Raghupathy <54129659+swaroopar@users.noreply.github.com>
2026-07-24 10:35:48 +0200

    feature/upgradeJetBrainsPlatform (#124)
    * upgrade jetbrains platform library version
    
    * remove redundant comments
    
    * upgrade jetbrains platform library version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "2.3.0"
-    id("org.jetbrains.intellij.platform") version "2.6.0"
+    id("org.jetbrains.kotlin.jvm") version "2.4.0"
+    id("org.jetbrains.intellij.platform") version "2.18.1"
 }
 
 group = providers.gradleProperty("pluginGroup").get()
